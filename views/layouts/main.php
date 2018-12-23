387e89bef123d54f7e57c513aac24c84ac240824
Ersted4 <37535522+Ersted4@users.noreply.github.com>
2018-12-23 21:52:32 +0300

    views/site
    Views

---
+++ views/layouts/main.php
@@ -0,0 +1,108 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\assets\PublicAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\helpers\Url;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+PublicAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <link rel="shortcut icon" href="/1.jpg" type="image/jpg">
+
+    <title>Огонёк</title>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<nav class="navbar main-menu navbar-default">
+    <div class="container">
+        <div class="menu-content">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/"><img src="/public/images/logo.jpg" alt=""></a>
+            </div>
+
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                <ul class="nav navbar-nav text-uppercase">
+                    <li><a data-toggle="dropdown" class="dropdown-toggle" href="/">Главная</a>
+
+                    </li>
+                </ul>
+                <div class="i_con">
+                    <ul class="nav navbar-nav text-uppercase">
+                        <?php if(Yii::$app->user->isGuest):?>
+                            <li><a href="<?= Url::toRoute(['auth/login'])?>">Войти</a></li>
+                            <li><a href="<?= Url::toRoute(['auth/signup'])?>">Регистрация</a></li>
+                        <?php else: ?>
+                            <?= Html::beginForm(['/auth/logout'], 'post')
+                            . Html::submitButton(
+                                'Выйти (' . Yii::$app->user->identity->name . ')',
+                                ['class' => 'btn btn-link logout', 'style'=>"padding-top:10px;"]
+                            )
+                            . Html::endForm() ?>
+                        <?php endif;?>
+                    </ul>
+                </div>
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+
+<?= $content ?>
+
+
+<footer class="footer-widget-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <aside class="footer-widget">
+                    <div class="about-img"><img src="/public/images/logo.jpg" alt=""></div>
+                    <div class="about-content">
+                    </div>
+                    <div class="address">
+                        <h4 class="text-uppercase">Контактная информация:</h4>
+
+                        <p> 11111/11 ТТ ТТТТТТТ, ТТ, ТТ</p>
+
+                        <p> Phone: +111 111 11111</p>
+
+                        <p>mytreasure.com</p>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
